dbae0cd5c1b0a1d407f9936f335351e29800c81f
Michal Grecner <grecnermichal@gmail.com>
2022-08-07 23:02:57 +0200

    first final version with GUI

--- script.js
+++ script.js
@@ -1,81 +1,132 @@
+let round = 0;
+let scorePlayer = 0;
+let scoreComputer = 0;
+let gameEnable = true //set to false after last round
+
+game()
+// main function
+function game(){
+        const buttonsPlayer = document.querySelectorAll(".btnPlayer"); 
+        buttonsPlayer.forEach((btn)=>{
+        btn.addEventListener("click",(e)=>{
+            if (gameEnable){ 
+            round ++;
+            if(round < 5){
+                let playerChoice = e.target.innerHTML.toLowerCase(); //get rock/paper/scissors from inner html of div
+                let computerChoice = getComputerChoice();
+                playRound(playerChoice, computerChoice); 
+            } else{
+                evaluation(scorePlayer, scoreComputer) //get the winner after lst round
+            }
+            score(); 
+            roundNm();
+        }
+        })
+    })
+}
+         
+
+function evaluation(scorePlayer, scorePC){
+    const Btn = document.querySelectorAll(".button");
+    Btn.forEach((x)=>{
+    x.style.pointerEvents = "none" //prevent buttton from transform: scale 1.1 when hover
+    })
+    let statusText = document.querySelector(".statusText")
+    if(scorePlayer==scorePC){
+        statusText.innerHTML= "No winner of match"
+    } else if(scorePlayer>scorePC){
+        statusText.innerHTML="you win the match!"
+    } else{
+        statusText.innerHTML="you lost the match!"
+    }
+    gameEnable = false 
+    document.querySelector(".playAgain").style.cssText = "display: block" //play again button appears
+}
+
+
 function getComputerChoice(){
     const choices=["rock", "paper", "scissors"] 
     let index = Math.floor(Math.random()*3)
     return choices[index]
 }
 
-
+//finds winner each round
 function playRound(playerSelection, computerSelection){
-    let result =""
-    console.log("player: "+playerSelection)
-    console.log("computer: "+computerSelection)
-    if(playerSelection==false){
-        result="wrong input, try again!!!"
-    }
-    else if(playerSelection==computerSelection){
-        result = "no winner this time"
+     if(playerSelection==computerSelection){
+        draw(playerSelection, computerSelection)
     }
     else if(playerSelection=="rock") {
         if(computerSelection=="paper"){
-            result = "you lost! paper beats rock!"
-            scorePC ++
+            computerWin(playerSelection, computerSelection)
         } else{
-            result = "you won, congrats! rock beats scissors"
-            scorePlayer ++
+            playerWin(playerSelection, computerSelection)
         }
     }
     else if(playerSelection=="scissors"){
         if(computerSelection=="rock"){
-            result = "you lost! rock beats scissors"
-            scorePC ++
+            computerWin(playerSelection, computerSelection)
         } else {
-            result = "you won, congrats! scissors beats paper"
-            scorePlayer ++
+            playerWin(playerSelection, computerSelection)
         }
     }
     else if(playerSelection=="paper"){
         if(computerSelection=="rock"){
-            result = "you won, congrats! paper beats rock"
-            scorePlayer ++
+            playerWin(playerSelection, computerSelection)
         } else {
-            result = "you lost! scissors beats paper!"
-            scorePC ++
+            computerWin(playerSelection, computerSelection)
 
         }
     }
-    return result
+    
 }
 
 
-
-function getPlayerChoice(){
-    const choices=["rock", "paper", "scissors"]
-    //const playerChoice = prompt("rock scissors or paper? whats your move?")
-    if(choices.includes(playerChoice.toLowerCase()) == false){
-        return false
-    } else {
-    return playerChoice.toLowerCase()
-    }
+function playerWin(playerChoice, computerChoice){
+    scorePlayer +=1;
+    let statusPlayer = document.querySelector(".statusPlayer");
+    let statusComputer = document.querySelector(".statusComputer");
+    let statusText = document.querySelector(".statusText")
+    statusPlayer.style.cssText = "border-color: green";
+    statusPlayer.innerHTML = playerChoice.toUpperCase();
+    statusComputer.style.cssText = "border-color: red";
+    statusComputer.innerHTML = computerChoice.toUpperCase();
+    statusText.innerHTML = "YOU WIN"
 }
 
-let scorePlayer = 0
-let scorePC = 0
-
-function game(){
-    for (let i = 0; i<5; i++){
-        console.log(playRound(getPlayerChoice(), getComputerChoice()))
-        console.log("round number:" + Number(i+1))
-    }
-    console.log("score player: "+ scorePlayer)
-    console.log("score PC: " + scorePC)
-    if(scorePlayer==scorePC){
-        console.log("No winner of match")
-    } else if(scorePlayer>scorePC){
-        console.log("congrats! you won whole match")
-    } else{
-        console.log("you lost whole match! you should try again!!!")
-    }
+function computerWin(playerChoice, computerChoice){
+    scoreComputer +=1;
+    let statusPlayer = document.querySelector(".statusPlayer");
+    let statusComputer = document.querySelector(".statusComputer")
+    let statusText  =document.querySelector(".statusText")
+    statusPlayer.style.cssText = "border-color: red";
+    statusPlayer.innerHTML = playerChoice.toUpperCase();
+    statusComputer.style.cssText = "border-color: green";
+    statusComputer.innerHTML = computerChoice.toUpperCase();
+    statusText.innerHTML = "YOU LOSE"
 }
 
-game()
+
+function draw(playerChoice, computerChoice){
+    
+    let statusPlayer = document.querySelector(".statusPlayer");
+    let statusComputer = document.querySelector(".statusComputer");
+    let statusText = document.querySelector(".statusText")
+    statusPlayer.style.cssText = "border-color: rgb(226, 226, 226)";
+    statusPlayer.innerHTML = playerChoice.toUpperCase();
+    statusComputer.style.cssText = "border-color: rgb(226, 226, 226)";
+    statusComputer.innerHTML = computerChoice.toUpperCase();
+    statusText.innerHTML = "DRAW"
+}
+
+
+function score(){
+    let score = document.querySelector(".score")
+    score.innerHTML= `${scorePlayer} - ${scoreComputer}`
+}
+
+
+function roundNm(){
+    let roundTag = document.querySelector(".round")
+    roundTag.innerHTML= `ROUND ${round} / 5`
+}
 
